b4da3d94d508309f1573b35398bb1076f8607434
Benjamín Starý <stary.benjamin@gmail.com>
2020-03-20 15:44:25 +0100

    webpack, babel, react, bulma (not working custom variables), loaders, etc

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new CopyWebpackPlugin([{
+      from: 'public/index.html',
+
+    }]),
+  ],
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
+}
